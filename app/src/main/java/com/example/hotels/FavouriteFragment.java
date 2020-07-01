9309b7f551c9c2a6ace731cf1989576d11439ff5
Vrishank <vrishank98@gmail.com>
2020-07-01 14:09:00 +0530

    View in frag
    Signed-off-by: Vrishank <vrishank98@gmail.com>

--- app/src/main/java/com/example/hotels/FavouriteFragment.java
+++ app/src/main/java/com/example/hotels/FavouriteFragment.java
@@ -14,6 +14,9 @@ public class FavouriteFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_favourite,container,false);
+        View view =  inflater.inflate(R.layout.fragment_favourite,container,false);
+
+
+        return view;
     }
 }
