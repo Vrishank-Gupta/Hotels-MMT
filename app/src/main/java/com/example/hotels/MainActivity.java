793f1c2524a2ce0a609d8aff4c830b625c5f1f6a
Vrishank <vrishank98@gmail.com>
2020-07-01 14:01:02 +0530

    Date
    Signed-off-by: Vrishank <vrishank98@gmail.com>

--- app/src/main/java/com/example/hotels/MainActivity.java
+++ app/src/main/java/com/example/hotels/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.hotels;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
--- app/src/main/java/com/example/hotels/SearchFragment.java
+++ app/src/main/java/com/example/hotels/SearchFragment.java
@@ -1,19 +1,77 @@
 package com.example.hotels;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class SearchFragment extends Fragment {
 
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    ImageView dateIcon;
+    TextView tvDate;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search,container,false);
+        View view = inflater.inflate(R.layout.fragment_search,container,false);
+
+        Toast.makeText(getContext(), "Hola", Toast.LENGTH_SHORT).show();
+
+        dateIcon = view.findViewById(R.id.dateIcon);
+        tvDate = view.findViewById(R.id.tvDate);
+
+        Date c = Calendar.getInstance().getTime();
+        SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        tvDate.setText(df.format(c));
+
+        dateIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year=cal.get(Calendar.YEAR);
+                int month=cal.get(Calendar.MONTH);
+                int day=cal.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog = new DatePickerDialog(getContext(),android.R.style.Theme_Holo_Light_Dialog_MinWidth,mDateSetListener,year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+
+        mDateSetListener=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+
+                String d = String.valueOf(day);
+                if(day<10)
+                    d = "0"+d;
+
+                month=month+1;
+                String m = String.valueOf(month);
+                if(month < 10)
+                    m="0"+m;
+
+                String date=d+"/"+m+"/"+year;
+                tvDate.setText(date);
+            }
+        };
+
+
+        return  view;
     }
 }
